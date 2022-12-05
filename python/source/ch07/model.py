17bdf23a4c49ba64074e89d30bd0c60cdccc21bd
Raman Jafarau <drf95@mail.ru>
2022-12-06 01:32:21 +0300

    Continue chapter 07 Context-Aware Text Analysis

--- python/source/ch07/model.py
+++ python/source/ch07/model.py
@@ -143,42 +143,42 @@
 #         return pow(2.0, self.entropy(text))
 
 
-class AddKNgramModel(BaseNgramModel):
-    """
-    Provides Add-k-smoothed scores.
-    """
+# class AddKNgramModel(BaseNgramModel):
+#     """
+#     Provides Add-k-smoothed scores.
+#     """
 
-    def __init__(self, k, *args):
-        """
-        Expects an input value, k, a number by which
-        to increment word counts during scoring.
-        """
-        super(AddKNgramModel, self).__init__(*args)
+#     def __init__(self, k, *args):
+#         """
+#         Expects an input value, k, a number by which
+#         to increment word counts during scoring.
+#         """
+#         super(AddKNgramModel, self).__init__(*args)
 
-        self.k = k
-        self.k_norm = len(self.ngram_counter.vocabulary) * k
+#         self.k = k
+#         self.k_norm = len(self.ngram_counter.vocabulary) * k
 
-    def score(self, word, context):
-        """
-        With Add-k-smoothing, the score is normalized with
-        a k value.
-        """
-        context = self.check_context(context)
-        context_freqdist = self.ngrams[context]
-        word_count = context_freqdist[word]
-        context_count = context_freqdist.N()
-        return (word_count + self.k) / \
-               (context_count + self.k_norm)
+#     def score(self, word, context):
+#         """
+#         With Add-k-smoothing, the score is normalized with
+#         a k value.
+#         """
+#         context = self.check_context(context)
+#         context_freqdist = self.ngrams[context]
+#         word_count = context_freqdist[word]
+#         context_count = context_freqdist.N()
+#         return (word_count + self.k) / \
+#                (context_count + self.k_norm)
 
 
-class LaplaceNgramModel(AddKNgramModel):
-    """
-    Implements Laplace (add one) smoothing.
-    Laplace smoothing is the base case of Add-k smoothing,
-    with k set to 1.
-    """
-    def __init__(self, *args):
-        super(LaplaceNgramModel, self).__init__(1, *args)
+# class LaplaceNgramModel(AddKNgramModel):
+#     """
+#     Implements Laplace (add one) smoothing.
+#     Laplace smoothing is the base case of Add-k smoothing,
+#     with k set to 1.
+#     """
+#     def __init__(self, *args):
+#         super(LaplaceNgramModel, self).__init__(1, *args)
 
 
 class KneserNeyModel(BaseNgramModel):
