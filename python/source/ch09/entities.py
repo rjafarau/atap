d4ce279191dd296a6786b5873d44c8709a23c50d
Raman Jafarau <drf95@mail.ru>
2023-03-14 02:02:54 +0300

    Continue chapter 09 Graph Analysis of Text

--- python/source/ch09/entities.py
+++ python/source/ch09/entities.py
@@ -1,48 +1,48 @@
-import spacy
+# import spacy
 import heapq
-import itertools
-import networkx as nx
+# import itertools
+# import networkx as nx
 
 from operator import itemgetter
 
-from reader import PickledCorpusReader
+# from reader import PickledCorpusReader
 
-nlp = spacy.load('en')
+# nlp = spacy.load('en')
 
-GOOD_ENTS = ['PERSON', 'NORP', 'FACILITY', 'ORG', 'GPE', 'LOC',
-             'PRODUCT', 'EVENT', 'WORK_OF_ART', 'LANGUAGE']
+# GOOD_ENTS = ['PERSON', 'NORP', 'FACILITY', 'ORG', 'GPE', 'LOC',
+#              'PRODUCT', 'EVENT', 'WORK_OF_ART', 'LANGUAGE']
 
-def entities(sent):
-    doc = nlp(sent)
-    for ent in doc.ents:
-        #  filter out non social entities
-        if ent.label_ in GOOD_ENTS:
-            return ent.text, ent.label_
-        else:
-            pass
+# def entities(sent):
+#     doc = nlp(sent)
+#     for ent in doc.ents:
+#         #  filter out non social entities
+#         if ent.label_ in GOOD_ENTS:
+#             return ent.text, ent.label_
+#         else:
+#             pass
 
-def pairs(doc):
-    candidates = [
-        entities(' '.join(word for word, tag in sent))
-        for para in doc for sent in para
-    ]
+# def pairs(doc):
+#     candidates = [
+#         entities(' '.join(word for word, tag in sent))
+#         for para in doc for sent in para
+#     ]
 
-    doc_entities = [
-        entity for entity in candidates if entity is not None
-    ]
+#     doc_entities = [
+#         entity for entity in candidates if entity is not None
+#     ]
 
-    return list(itertools.permutations(set(doc_entities), 2))
+#     return list(itertools.permutations(set(doc_entities), 2))
 
 
-def graph(docs):
-    G = nx.Graph()
-    for doc in docs:
-        for pair in pairs(doc):
-            if (pair[0][0], pair[1][0]) in G.edges():
-                G.edges[(pair[0][0], pair[1][0])]['weight'] += 1
-            else:
-                G.add_edge(pair[0][0], pair[1][0], weight=1)
-    return G
+# def graph(docs):
+#     G = nx.Graph()
+#     for doc in docs:
+#         for pair in pairs(doc):
+#             if (pair[0][0], pair[1][0]) in G.edges():
+#                 G.edges[(pair[0][0], pair[1][0])]['weight'] += 1
+#             else:
+#                 G.add_edge(pair[0][0], pair[1][0], weight=1)
+#     return G
 
 def nbest_centrality(G, metric, n=10, attr="centrality", **kwargs):
     # Compute the centrality scores for each vertex
@@ -59,8 +59,8 @@ def nbest_centrality(G, metric, n=10, attr="centrality", **kwargs):
     return G
 
 
-if __name__ == '__main__':
-    corpus = PickledCorpusReader("../corpus")
-    docs = corpus.docs()
-    G = graph(docs)
+# if __name__ == '__main__':
+#     corpus = PickledCorpusReader("../corpus")
+#     docs = corpus.docs()
+#     G = graph(docs)
 
