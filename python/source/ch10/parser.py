06e3b77098f07070c87c5379b989278d03ded3f1
Raman Jafarau <drf95@mail.ru>
2023-08-28 03:21:28 +0300

    Continue chapter 10 Chatbots

--- python/source/ch10/parser.py
+++ python/source/ch10/parser.py
@@ -1,8 +1,8 @@
-import spacy
-import pprint
+# import spacy
+# import pprint
 import argparse
-from nltk.tree import Tree
-from nltk.parse.stanford import StanfordParser
+# from nltk.tree import Tree
+# from nltk.parse.stanford import StanfordParser
 
 
 ##########################################################################
@@ -10,35 +10,35 @@ from nltk.parse.stanford import StanfordParser
 ##########################################################################
 
 # Required: python -m spacy download en
-spacy_nlp = spacy.load("en")
+# spacy_nlp = spacy.load("en")
 
-def spacy_tree(sent):
-    """
-    Get the SpaCy dependency tree structure
-    :param sent: string
-    :return: None
-    """
-    doc = spacy_nlp(sent)
-    pprint.pprint(doc.print_tree())
+# def spacy_tree(sent):
+#     """
+#     Get the SpaCy dependency tree structure
+#     :param sent: string
+#     :return: None
+#     """
+#     doc = spacy_nlp(sent)
+#     pprint.pprint(doc.print_tree())
 
-def nltk_spacy_tree(sent):
-    """
-    Visually inspect the SpaCy dependency tree with nltk.tree
-    :param sent: string
-    :return: None
-    """
-    doc = spacy_nlp(sent)
-    def token_format(token):
-        return "_".join([token.orth_, token.tag_, token.dep_])
+# def nltk_spacy_tree(sent):
+#     """
+#     Visually inspect the SpaCy dependency tree with nltk.tree
+#     :param sent: string
+#     :return: None
+#     """
+#     doc = spacy_nlp(sent)
+#     def token_format(token):
+#         return "_".join([token.orth_, token.tag_, token.dep_])
 
-    def to_nltk_tree(node):
-        if node.n_lefts + node.n_rights > 0:
-            return Tree(token_format(node),[to_nltk_tree(child) for child in node.children])
-        else:
-            return token_format(node)
+#     def to_nltk_tree(node):
+#         if node.n_lefts + node.n_rights > 0:
+#             return Tree(token_format(node),[to_nltk_tree(child) for child in node.children])
+#         else:
+#             return token_format(node)
 
-    tree = [to_nltk_tree(sent.root) for sent in doc.sents]
-    tree[0].draw()
+#     tree = [to_nltk_tree(sent.root) for sent in doc.sents]
+#     tree[0].draw()
 
 def id_measure_quest(sent):
     """
@@ -62,29 +62,29 @@ def id_measure_quest(sent):
 ##########################################################################
 # Required: download Stanford jar dependencies
 # https://stackoverflow.com/questions/13883277/stanford-parser-and-nltk
-stanford_parser = StanfordParser(
-    model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
-)
+# stanford_parser = StanfordParser(
+#     model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
+# )
 
-def nltk_stanford_parse(sent):
-    """
-    Use Stanford pretrained model to extract dependency tree
-    for use by other methods
-    :param sent: str
-    :return: list of trees
-    """
-    parse = stanford_parser.raw_parse(sent)
-    return list(parse)
+# def nltk_stanford_parse(sent):
+#     """
+#     Use Stanford pretrained model to extract dependency tree
+#     for use by other methods
+#     :param sent: str
+#     :return: list of trees
+#     """
+#     parse = stanford_parser.raw_parse(sent)
+#     return list(parse)
 
-def nltk_stanford_tree(sent):
-    """
-    Visually inspect the Stanford dependency tree as an image
-    :param sent: str
-    :return: NLTK image
-    """
-    parse = stanford_parser.raw_parse(sent)
-    tree = list(parse)
-    tree[0].draw()
+# def nltk_stanford_tree(sent):
+#     """
+#     Visually inspect the Stanford dependency tree as an image
+#     :param sent: str
+#     :return: NLTK image
+#     """
+#     parse = stanford_parser.raw_parse(sent)
+#     tree = list(parse)
+#     tree[0].draw()
 
 
 def question_type(tree):
@@ -115,28 +115,28 @@ def question_type(tree):
 
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # Do some command line argument parsing
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-p', '--parser', choices={'nltk', 'syntaxnet'},
-        default='nltk', help='select parser to use',
-    )
-    parser.add_argument('sents', nargs="+", help='sentences to parse, surround by quotes')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     '-p', '--parser', choices={'nltk', 'syntaxnet'},
+    #     default='nltk', help='select parser to use',
+    # )
+    # parser.add_argument('sents', nargs="+", help='sentences to parse, surround by quotes')
+    # args = parser.parse_args()
     # print(args.sents)
 
-    if args.parser == 'syntaxnet':
-        raise NotImplementedError("not done yet")
-    elif args.parser == 'nltk':
-        for sent in args.sents:
+    # if args.parser == 'syntaxnet':
+    #     raise NotImplementedError("not done yet")
+    # elif args.parser == 'nltk':
+    #     for sent in args.sents:
             ## Visualize the trees
             # nltk_spacy_tree(sent) # "Dependency" tree
             # nltk_stanford_tree(sent) # "Constituency" tree
 
             # Get Stanford dependency tree as a list
-            parse = nltk_stanford_parse(sent)
+            # parse = nltk_stanford_parse(sent)
             print(question_type(parse))
 
-    else:
-        raise TypeError("'{}' is not a valid parser".format(args.parser))
+    # else:
+    #     raise TypeError("'{}' is not a valid parser".format(args.parser))
