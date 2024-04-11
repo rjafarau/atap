19ac0fe0c4e2380bc2918731abba164b6039dba3
Raman Jafarau <drf95@mail.ru>
2024-04-11 23:14:19 +0000

    Continue chapter 12 Deep Learning and Beyond

--- python/source/chapter_12/reader.py
+++ python/source/chapter_12/reader.py
@@ -1,12 +1,12 @@
 import time
-# import pickle
+import pickle
 import sqlite3
 
 import nltk
 
-# from nltk.corpus.reader.api import CorpusReader
+from nltk.corpus.reader.api import CorpusReader
 
-# PKL_PATTERN = r'(?!\.)[\w\s\d\-]+\.pickle'
+PKL_PATTERN = r'(?!\.)[\w\s\d\-]+\.pickle'
 
 
 class SqliteCorpusReader(object):
@@ -150,76 +150,139 @@ class SqliteCorpusReader(object):
         ).format(**self.describe())
 
 
-if __name__ == '__main__':
-    corpus = SqliteCorpusReader('/home/python/project/data/database.sqlite')
-    print(corpus.describes())
+class PickledCorpusReader(CorpusReader):
 
-# class PickledReviewsReader(CorpusReader):
-#     def __init__(self, root, fileids=PKL_PATTERN, **kwargs):
-#         """
-#         Initialize the corpus reader
-#         """
-#         CorpusReader.__init__(self, root, fileids, **kwargs)
+    def __init__(self, root, fileids=PKL_PATTERN, **kwargs):
+        """
+        Initialize the corpus reader
+        """
+        CorpusReader.__init__(self, root, fileids, **kwargs)
 
-#     def texts_scores(self, fileids=None):
-#         """
-#         Returns the document loaded from a pickled object for every file in
-#         the corpus. Similar to the SqliteCorpusReader, this uses a generator
-#         to achieve memory safe iteration.
-#         """
-#         # Create a generator, loading one document into memory at a time.
-#         for path, enc, fileid in self.abspaths(fileids, True, True):
-#             with open(path, 'rb') as f:
-#                 yield pickle.load(f)
+    def docs(self, fileids=None):
+        """
+        Returns the document loaded from a pickled object for every file in
+        the corpus. Similar to the SqliteCorpusReader, this uses a generator
+        to achieve memory safe iteration.
+        """
+        # Create a generator, loading one document into memory at a time.
+        for path in self.abspaths(fileids):
+            with open(path, 'rb') as f:
+                yield pickle.load(f)
 
-#     def reviews(self, fileids=None):
-#         """
-#         Returns a generator of paragraphs where each paragraph is a list of
-#         sentences, which is in turn a list of (token, tag) tuples.
-#         """
-#         for text,score in self.texts_scores(fileids):
-#             yield text
+    def scores(self, fileids=None):
+        """
+        Return the scores
+        """
+        for doc in self.docs(fileids):
+            yield doc['score']
 
-#     def scores(self, fileids=None):
-#         """
-#         Return the scores
-#         """
-#         for text,score in self.texts_scores(fileids):
-#             yield score
+    def tagged_texts(self, fileids=None):
+        """
+        Returns a generator of texts where each text is a list of paragraphs,
+        which is in turn a list of sentences, which is in turn a list of
+        (word, tag) tuples.
+        """
+        for doc in self.docs(fileids):
+            yield doc['text']
 
-#     def paras(self, fileids=None):
-#         """
-#         Returns a generator of paragraphs where each paragraph is a list of
-#         sentences, which is in turn a list of (token, tag) tuples.
-#         """
-#         for review in self.reviews(fileids):
-#             for paragraph in review:
-#                 yield paragraph
+    def tagged_paras(self, fileids=None):
+        """
+        Returns a generator of paragraphs where each paragraph is a list of
+        sentences, which is in turn a list of (word, tag) tuples.
+        """
+        for text in self.tagged_texts(fileids):
+            for paragraph in text:
+                yield paragraph
 
-#     def sents(self, fileids=None):
-#         """
-#         Returns a generator of sentences where each sentence is a list of
-#         (token, tag) tuples.
-#         """
-#         for paragraph in self.paras(fileids):
-#             for sentence in paragraph:
-#                 yield sentence
+    def paras(self, fileids=None):
+        """
+        Returns a generator of paragraphs where each paragraph is a list of
+        sentences, which is in turn a list of words.
+        """
+        for tagged_para in self.tagged_paras(fileids):
+            yield [[word for word, tag in tagged_sent]
+                   for tagged_sent in tagged_para]
 
-#     def tagged(self, fileids=None):
-#         for sent in self.sents(fileids):
-#             for token in sent:
-#                 yield token
+    def tagged_sents(self, fileids=None):
+        """
+        Returns a generator of sentences where each sentence is a list of
+        (word, tag) tuples.
+        """
+        for tagged_para in self.tagged_paras(fileids):
+            for tagged_sent in tagged_para:
+                yield tagged_sent
 
-#     def words(self, fileids=None):
-#         """
-#         Returns a generator of (token, tag) tuples.
-#         """
-#         for token in self.tagged(fileids):
-#             yield token[0]
+    def sents(self, fileids=None):
+        """
+        Returns a generator of sentences where each sentence is a list of
+        words.
+        """
+        for tagged_sent in self.tagged_sents(fileids):
+            yield [word for word, tag in tagged_sent]
 
+    def tagged_words(self, fileids=None):
+        """
+        Returns a generator of (word, tag) tuples.
+        """
+        for sent in self.tagged_sents(fileids):
+            for word, tag in sent:
+                yield word, tag
 
-# if __name__ == '__main__':
-#     # Download the data from https://www.kaggle.com/nolanbconaway/pitchfork-data/data
-#     # preprocess by running preprocess.py to produce pickled corpus
-#     reader = PickledReviewsReader('../review_corpus_proc')
-#     print(len(list(reader.reviews())))
+    def words(self, fileids=None):
+        """
+        Returns a generator of words.
+        """
+        for word, tag in self.tagged_words(fileids):
+            yield word
+
+    def describe(self, fileids=None):
+        """
+        Performs a single pass of the corpus and returns a dictionary with a
+        variety of metrics concerning the state of the corpus.
+        """
+        started = time.perf_counter()
+
+        # Structures to perform counting.
+        counts = nltk.FreqDist()
+        words = nltk.FreqDist()
+
+        # Perform single pass over paragraphs, tokenize and count
+        for para in self.tagged_paras(fileids):
+            counts['paras'] += 1
+
+            for sent in para:
+                counts['sents'] += 1
+
+                for word, tag in sent:
+                    counts['words'] += 1
+                    words[word] += 1
+
+        # Compute the number of files in the corpus
+        n_fileids = len(fileids or self.fileids())
+
+        # Return data structure with information
+        return {
+            'files': n_fileids,
+            'paras': counts['paras'],
+            'sents': counts['sents'],
+            'words': counts['words'],
+            'vocab': len(words),
+            'lexdiv': counts['words'] / len(words),
+            'ppdoc': counts['paras'] / n_fileids,
+            'sppar': counts['sents'] / counts['paras'],
+            'secs': time.perf_counter() - started,
+        }
+
+    def describes(self):
+        """
+        Returns a string representation of the describe command.
+        """
+        return (
+            "Corpus contains {files:,} files.\n"
+            "Structured as:\n"
+            "    {paras:,} paragraphs ({ppdoc:0,.3f} mean paragraphs per file)\n"
+            "    {sents:,} sentences ({sppar:0,.3f} mean sentences per paragraph).\n"
+            "Word count of {words:,} with a vocabulary of {vocab:,} "
+            "({lexdiv:0,.3f} lexical diversity).\n"
+            "Corpus scan took {secs:0,.3f} seconds."
+        ).format(**self.describe())
